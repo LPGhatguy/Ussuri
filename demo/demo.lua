7b6170d52eecf333de36fdb5cfbd9ec3902a9985
Lucien Greathouse <lpghatguy@lpghatguy.com>
2012-11-18 05:33:32 -0700

    Switched around the demo

--- demo/demo.lua
+++ demo/demo.lua
@@ -1,12 +1,18 @@
-local ld25_engine = require("engine.core")
-ld25_engine:init()
+local engine = require("engine.core")
 
 function love.load()
-	print(ld25_engine.lib.utility)
+	engine:init()
 end
 
 function love.keypressed(key, unicode)
 	if (key == "escape") then
 		love.event.push("quit")
 	end
+end
+
+function love.draw()
+end
+
+function love.quit()
+	engine:close()
 end
