aacc281a7bf5f43ab1341b3a9e59174c50dad665
Lucien Greathouse <lpghatguy@lpghatguy.com>
2012-11-17 23:24:33 -0700

    Update engine core to add core library loading and make config less stupid

--- engine/core.lua
+++ engine/core.lua
@@ -1,20 +1,31 @@
 local engine_core = {}
 local lib, config = {}, {}
 local engine_path = debug.getinfo(1).short_src:match("([^%.]*)[\\/][^%.]*%..*$"):gsub("[\\/]", ".") .. "."
+config.engine_path = engine_path
 
-engine_core.config_get = function(key)
-	return config[key]
+local function load_lib(load)
+	local name = load:gsub("^:", "")
+	local loaded = require(load:gsub("^:", config.engine_path)):init(engine_core)
+	lib[name] = loaded
+
+	return loaded
 end
 
-engine_core.config_set = function(key, value)
-	config[key] = value
+local function batch_load_lib(batch)
+	for index, lib_name in next, batch do
+		load_lib(lib_name)
+	end
 end
 
 engine_core.init = function(self, glib)
-	lib = glib
+	lib = glib or lib
+	engine_core.lib = lib
 
 	config = require(engine_path .. "config")
 	config.engine_path = config.engine_path or engine_path
+	engine_core.config = config
+
+	batch_load_lib(config.core_lib)
 
 	return self
 end
