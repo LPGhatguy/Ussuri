3f3a8b0478440cd89ccd1f50a320bc7b28219a51
Lucien Greathouse <lpghatguy@lpghatguy.com>
2012-11-17 23:24:49 -0700

    Update engine "demo"

--- main.lua
+++ main.lua
@@ -1,18 +1,12 @@
-local IS_LOVE = (love and true)
-
 local ld25_engine = require("engine.core")
 ld25_engine:init()
 
 function love.load()
-	print(ld25_engine.config_get("engine_path"))
+	print(ld25_engine.lib.utility)
 end
 
-if (IS_LOVE) then
-
-	function love.keypressed(key, unicode)
-		if (key == "escape") then
-			love.event.push("quit")
-		end
+function love.keypressed(key, unicode)
+	if (key == "escape") then
+		love.event.push("quit")
 	end
-
 end
