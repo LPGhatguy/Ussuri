b298991decfe2e3385a6a7892127543acdf486d1
Lucien Greathouse <lpghatguy@lpghatguy.com>
2012-11-18 05:33:48 -0700

    Restructured engine.config

--- engine/config.lua
+++ engine/config.lua
@@ -1,7 +1,10 @@
-local config = {
+return
+{
 	engine_path = nil, --filled in by engine initialization
-	core_lib = {":utility"},
-	engine_lib = {}
+	core_lib = {":core.utility", ":core.oop", ":core.log", ":core.lib_manage"},
+	engine_lib = {},
+	record_logs = false
+},
+{
+	engine_version = {0, 0, 1}
 }
-
-return config
