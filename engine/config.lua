55b01005616520ed8b5cc43ee07b5ab4bbe33c08
Lucien Greathouse <lpghatguy@lpghatguy.com>
2012-11-18 14:35:49 -0700

    Restructure configuration

--- engine/config.lua
+++ engine/config.lua
@@ -1,10 +1,12 @@
 return
 {
+	--core
 	engine_path = nil, --filled in by engine initialization
-	core_lib = {":core.utility", ":core.oop", ":core.log", ":core.lib_manage"},
+	--core.lib_manage
+	core_lib = {":core.utility", ":core.oop", ":core.logging", ":core.lib_manage"},
 	engine_lib = {},
-	record_logs = false
-},
-{
-	engine_version = {0, 0, 1}
+	--core.logging
+	log_realtime_enabled = true,
+	log_history_enabled = true,
+	log_recording_enabled = false
 }
