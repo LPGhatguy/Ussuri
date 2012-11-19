f0971f3fcef60fdab4c918b8f7fca18bbaaf82f3
Lucien Greathouse <lpghatguy@lpghatguy.com>
2012-11-18 18:31:10 -0700

    Add event as core library

--- engine/config.lua
+++ engine/config.lua
@@ -3,7 +3,7 @@ return
 	--core
 	engine_path = nil, --filled in by engine initialization
 	--core.lib_manage
-	lib_core = {":core.utility", ":core.oop", ":core.logging", ":core.lib_manage"},
+	lib_core = {":core.utility", ":core.oop", ":core.logging", ":core.lib_manage", ":core.event"},
 	lib_engine = {},
 	--core.logging
 	log_realtime_enabled = true,
