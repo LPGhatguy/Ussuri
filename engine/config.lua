47d50df15c77da7cfdcef4044214cb14f104f4d7
Lucien Greathouse <lpghatguy@lpghatguy.com>
2012-12-22 01:31:09 -0700

    Add version to config, add engine.extend.delayed

--- engine/config.lua
+++ engine/config.lua
@@ -1,11 +1,12 @@
 return
 {
+	--details
+	version = 1.1,
 	--core
 	engine_path = nil, --filled in by engine initialization
 	--core.lib_manage
 	lib_core = {":core.utility", ":core.oop", ":core.logging", ":core.event", ":core.lib_manage"},
-	lib_engine = {
-	},
+	lib_engine = {":extend.event_def", ":extend.debris", ":extend.debris_manager", ":extend.delayed"},
 	--core.logging
 	log_realtime_enabled = true,
 	log_history_enabled = true,
