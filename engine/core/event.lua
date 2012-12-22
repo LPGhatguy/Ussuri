a18ce1befaf77c3fd690389d1c2b2e3c66588d95
Lucien Greathouse <lpghatguy@lpghatguy.com>
2012-12-22 01:32:25 -0700

    Overhaul event system
    -Change event format to use less indexing
    -Remove light events after doing benchmarking (regular events will be
    optimized even more soon)
    -Optimize event_pass object initialization
    -Allow batch event hooking
    -Allow automatic determination of events to hook to an object

--- engine/core/event.lua
+++ engine/core/event.lua
@@ -3,122 +3,103 @@ local lib = {}
 local engine
 
 event.events = {}
-event.events_light = {}
 
-event.event_get_handler_func = function(handler, event_name)
-	return handler["event_" .. event_name] or (handler.event and handler.event[event_name])
+event.event_handler_sorter = function(first, second)
+	return (first[3] or 0) > (second[3] > 0)
 end
 
-event.dict_to_array = function(dictionary)
-	local array = {}
-	local key = 1
-	for key, value in pairs(dictionary) do
-		array[key] = value
-		key = key + 1
+event.event_sort_handlers = function(self, event_name)
+	if (event_name) then
+		table.sort(self.events[event_name], self.event_handler_sorter)
+	else
+		for name, handlers in next, self.events do
+			table.sort(handlers, self.event_handler_sorter)
+		end
 	end
-	return array
 end
 
-event.event_hook = function(self, event_name, handler, priority)
+event.event_create = function(self, event_name)
+	self.events[event_name] = self.events[event_name] or {}
+end
+
+event.event_create_batch = function(self, ...)
+	for key, event_name in next, {...} do
+		self:event_create(event_name)
+	end
+end
+
+event.event_hook_auto = function(self, object, priority)
+	local object_type = type(object)
+
+	if (object_type == "table") then
+		local methods = object.event or object
+
+		for name, handler in next, methods do
+			table.insert(event, {object, handler, priority})
+		end
+	elseif (object_type == "function") then
+		for event_name, event in next, self.events do
+			table.insert(event, {object, object, priority})
+		end
+	end
+end
+
+event.event_hook_batch = function(self, object, handlers, priority)
+	if (type(handlers) == "table") then
+		for handler_id, handler_name in next, handlers do
+			self:event_hook(handler_name, object, nil, priority)
+		end
+	else
+		self:event_hook_auto(object, priority or handlers) --handlers is passed as priority instead
+	end
+end
+
+event.event_hook = function(self, event_name, object, handler, priority)
+	local handler = handler or (type(object) == "table") and ((object.event and object.event[event_name]) or object[event_name]) or object
 	local handlers = self.events[event_name] or {}
 	self.events[event_name] = handlers
 
-	if (handler) then
-		table.insert(handlers, priority or handler)
-	end
-end
-
-event.event_hook_light = function(self, event_name, handler, priority)
-	local handlers = self.events_light[event_name] or {}
-	self.events_light[event_name] = handlers
-
-	if (handler) then
-		table.insert(handlers, priority or handler)
+	if (object) then
+		table.insert(handlers, {object, handler, priority or 0})
 	end
 end
 
 event.event_trigger = function(self, event_name, arguments)
-	if (self.events_light[event_name]) then
-		self:event_trigger_light(event_name, arguments)
-	end
-
-	if (self.events[event_name]) then
-		self:event_trigger_full(event_name, arguments)
-	end
-end
-
-event.event_trigger_full = function(self, event_name, arguments)
 	local event_pass = self.event_pass:new(event_name, arguments)
 	local handlers = self.events[event_name]
 
 	if (handlers) then
 		for key, handler in next, handlers do
-			if (type(handler) == "function") then
-				handler(handler, event_pass)
-			elseif (type(handler) == "table") then
-				local handler_func = self.event_get_handler_func(handler, event_name)
+			local object = handler[1]
+			local method = handler[2]
 
-				if (handler_func) then
-					handler_func(handler, event_pass)
-				else
-					if (self.__logger) then
-						self:log_write("Could not find handler for", key, "of full event", event_name)
-					end
-				end
-			else
-				if (self.__logger) then
-					self:log_write("Unsupported handler type:", type(handler), "for", key, "of full event", event_name)
-				end
-			end
+			if (type(method) == "function") then
+				method(object, event_pass)
 
-			if (event_pass.cancel) then
-				break
-			end
-		end
-	else
-		if (self.__logger) then
-			self:log_write("Could not find full event", event_name)
-		end
-	end
-end
-
-event.event_trigger_light = function(self, event_name, arguments)
-	local handlers = self.events_light[event_name]
-	local arguments = arguments or {}
-
-	if (handlers) then
-		for key, handler in next, handlers do
-			if (type(handler) == "function") then
-				handler(handler, self.dict_to_array(arguments))
-			elseif (type(handler) == "table") then
-				local handler_func = self.event_get_handler_func(handler, event_name)
-
-				if (handler_func) then
-					handler_func(handler, self.dict_to_array(arguments))
-				else
-					if (self.__logger) then
-						self:log_write("Could not find handler for", key, "of light event", event_name)
-					end
-				end
-			else
-				if (self.__logger) then
-					self:log_write("Unsupported handler type:", type(handler), "for", key, "of light event", event_name)
+				if (event_pass.cancel) then
+					break
 				end
 			end
 		end
 	else
 		if (self.__logger) then
-			self:log_write("Could not find light event", event_name)
+			self:log_write("Could not find event", event_name)
 		end
 	end
+
+	return event_pass
 end
 
 event.event_pass = {}
 event.event_pass.new = function(self, event_name, arguments)
 	local pass = self:_new()
 
-	lib.utility.table_merge(arguments, pass)
-	pass.event_name = event_name
+	if (arguments) then
+		for key, value in next, arguments do
+			pass[key] = value
+		end
+	end
+
 	pass.cancel = false
 
 	return pass
