f6ac5374ddfd01c4328ef066355dd52d1c539698
Lucien Greathouse <lpghatguy@lpghatguy.com>
2012-12-22 22:00:52 -0700

    Switch from extend.delayed to extend.delay

--- engine/config.lua
+++ engine/config.lua
@@ -6,7 +6,7 @@ return
 	engine_path = nil, --filled in by engine initialization
 	--core.lib_manage
 	lib_core = {":core.utility", ":core.oop", ":core.logging", ":core.event", ":core.lib_manage"},
-	lib_engine = {":extend.event_def", ":extend.debris", ":extend.debris_manager", ":extend.delayed"},
+	lib_engine = {":extend.event_def", ":extend.debris", ":extend.debris_manager", ":extend.delay"},
 	--core.logging
 	log_realtime_enabled = true,
 	log_history_enabled = true,
