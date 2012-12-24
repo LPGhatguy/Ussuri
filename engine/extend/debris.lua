49dc5aed1024df68aec296a99b8dd4890cc83637
Lucien Greathouse <lpghatguy@lpghatguy.com>
2012-12-24 07:10:18 -0700

    Debris object fleshing out

--- engine/extend/debris.lua
+++ engine/extend/debris.lua
@@ -6,29 +6,37 @@ debris.debris_alive = true
 
 debris.event = {
 	update = function(self, event)
-		self:debris_step(event.delta)
+		event.unhook = self:debris_step(event.delta)
 	end
 }
 
 debris.debris_step = function(self, delta)
 	if (self.debris_alive) then
-		local expended = self.debris_expended + event.delta
+		local expended = self.debris_expended + delta
 		self.debris_expended = expended
 
 		if (expended > self.debris_lifetime) then
-			self:debris_destroy()
+			self.debris_alive = false
+			return not self:debris_destroy()
 		else
-			self:debris_update()
+			return self:debris_update()
 		end
 	end
 end
 
-debris.debris_destroy = function(self)
-	self.alive = false
+debris.new = function(self, lifetime, update, destroy)
+	local new = self:_new()
+
+	new.debris_lifetime = lifetime or new.debris_lifetime
+	new.debris_update = update or new.debris_update
+	new.debris_destroy = destroy or new.debris_destroy
+
+	return new
 end
 
 debris.init = function(self, engine)
 	self.debris_update = engine.lib.utility.do_nothing
+	self.debris_destroy = engine.lib.utility.do_nothing
 	engine.lib.oop:objectify(self)
 
 	return self
