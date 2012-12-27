c728950139982d7b5229339c9d420d8c933d6005
Lucien Greathouse <lpghatguy@lpghatguy.com>
2012-12-27 06:28:05 -0700

    Add mouse event support

--- engine/extend/event_def.lua
+++ engine/extend/event_def.lua
@@ -15,6 +15,22 @@ definitions.fire_keyup = function(self, key, unicode)
 	})
 end
 
+definitions.fire_mousedown = function(self, x, y, button)
+	return self:event_trigger("mousedown", {
+		x = x,
+		y = y,
+		button = button
+	})
+end
+
+definitions.fire_mouseup = function(self, x, y, button)
+	return self:event_trigger("mouseup", {
+		x = x,
+		y = y,
+		button = button
+	})
+end
+
 definitions.fire_update = function(self, delta)
 	return self:event_trigger("update", {
 		delta = delta
@@ -28,7 +44,7 @@ end
 definitions.init = function(self, engine)
 	lib = engine.lib
 
-	engine:event_create_batch("update", "draw", "keydown", "keyup", "quit")
+	engine:event_create_batch("update", "draw", "keydown", "keyup", "mousedown", "mouseup", "quit")
 	engine:inherit(self)
 
 	return self
