984df9c7ca00c6359e97afcf5ae3f2c04ff2994f
Lucien Greathouse <lpghatguy@lpghatguy.com>
2012-12-31 04:25:33 -0700

    Delete comment

--- main.lua
+++ main.lua
@@ -1,8 +1,2 @@
 --Load the engine demo
 require("demo.demo")
-
---[[
-This is the place to plop all your frontend coding. The demo is put
-in its own folder so it can include assets for itself while still
-being easy to remove.
-]]
