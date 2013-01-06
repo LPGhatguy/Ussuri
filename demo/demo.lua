8bc6e6daff35395c11a5853b0faa9fb4cb3ee050
Lucien Greathouse <lpghatguy@lpghatguy.com>
2013-01-06 02:01:09 -0700

    Update demo

--- demo/demo.lua
+++ demo/demo.lua
@@ -1,26 +1,26 @@
 --[[
-
+Ussuri Demo
+A small silly demo of the engine's capabilities
+Written by Lucien Greathouse
 ]]
 
-local lib, extend
+local lib, misc
 local engine = require("engine")
 
 function love.load()
+	love.graphics.setBackgroundColor(0, 70, 150)
+
 	print = function(...)
 		engine:log_writes("out", ...)
 	end
 
 	lib = engine.lib
-	extend = lib.extend
-
-	engine:event_hook("update", function(self)
-		love.graphics.setColor(extend.color.hsv(127.5 + 127.5 * math.sin(love.timer.getMicroTime() / 3), 255, 255))
-		love.graphics.rectangle("fill", 0, 0, 1024, 768)
-	end)
+	misc = lib.misc
 
 	engine:event_hook("keydown", function(self, event)
 		if (event.key == "escape") then
-			if (love.keyboard.isDown("lctrl")) then
+			if (love.keyboard.isDown("lshift")) then
+				print((lib.utility.table_tree(lib)))
 				engine.config.log_recording_enabled = true
 			end
 
@@ -32,8 +32,9 @@ function love.load()
 	engine:event_hook_auto(lib.debug.debug_monitor)
 	engine:event_hook_auto(lib.debug.console)
 
-	local sound = extend.sound_manager:new()
+	local sound = misc.sound_manager:new()
 
+	sound:disable_sound()
 	engine:event_hook_auto(sound)
 
 	sound:load_effect("demo/resource/bloop.ogg")
@@ -49,11 +50,6 @@ function love.load()
 		elseif (event.key == "return" and (love.keyboard.isDown("lctrl") or love.keyboard.isDown("rctrl"))) then
 			love.graphics.toggleFullscreen()
 		end
-	end)
-
-	engine:event_hook("draw", function()
-		love.graphics.setColor(255, 255, 255)
-		love.graphics.printf("Press space to bloop, press alt to toggle sound", 0, 750, 1024, "center")
 	end)
 end
 
