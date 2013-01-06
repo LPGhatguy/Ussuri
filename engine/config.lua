06e8a7a68d74616acedb1992122b70b13118668b
Lucien Greathouse <lpghatguy@lpghatguy.com>
2013-01-06 02:01:20 -0700

    Update config

--- engine/config.lua
+++ engine/config.lua
@@ -1,13 +1,13 @@
-return
-{
+return {
 	--details
 	version = 1.1,
 	--core
 	engine_path = nil, --filled in by engine initialization
 	--core.lib_manage
-	lib_core = {":core.utility", ":core.oop", ":core.logging", ":core.event", ":core.event_def", ":core.lib_manage"},
+	lib_core = {":core.utility", ":core.oop", ":core.logging", ":core.event", ":core.event_def", ":core.lib_manage", ":core.shorthand"},
 	lib_folders = {
 		[":extend"] = {},
+		[":misc"] = {},
 		[":debug"] = {},
 		[":input"] = {},
 		[":gui"] = {}
