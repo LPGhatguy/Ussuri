d72f4adfe396e132ecca2d28540d6fc90bc7ce1d
Lucien Greathouse <lpghatguy@lpghatguy.com>
2013-01-12 23:29:52 -0700

    Change shorthand meta initialization

--- engine/core/shorthand.lua
+++ engine/core/shorthand.lua
@@ -6,42 +6,52 @@ Written by Lucien Greathouse
 ]]
 
 local engine
-local lib
+local lib, tpop
 _ = {}
+
+local meta = {}
+
+meta.__call = function(self)
+	self = self or engine
+
+	return self.lib
+end
+
+meta.__add = function(self, message)
+	if (message) then
+		self:log_write(type(message) == "table" and unpack(message) or message)
+	end
+end
+
+meta.__sub = function(self, arg)
+	self:log_pop(arg)
+end
+
+meta.__mul = function(self, arg)
+	local count = tpop(arg)
+	local method = tpop(arg)
+
+	for i = 1, count do
+		method(i, unpack(arg))
+	end
+end
+
+meta.__pow = function(self, arg)
+	local count = tpop(arg)
+	local method = tpop(arg)
+
+	for i = 1, count do
+		method(unpack(arg))
+	end
+end
 
 _.init = function(self, engine)
 	lib = engine.lib
 	_ = engine
 
-	local tpop = lib.utility.table_pop
+	tpop = lib.utility.table_pop
 
-	setmetatable(engine, {
-		__call = function(self)
-			return self.lib
-		end,
-		__add = function(self, message)
-			self:log_write(type(message) == "table" and unpack(message) or message)
-		end,
-		__sub = function(self, arg)
-			self:log_pop(arg)
-		end,
-		__mul = function(self, arg)
-			local count = tpop(arg)
-			local method = tpop(arg)
-
-			for i = 1, count do
-				method(i, unpack(arg))
-			end
-		end,
-		__pow = function(self, arg)
-			local count = tpop(arg)
-			local method = tpop(arg)
-
-			for i = 1, count do
-				method(unpack(arg))
-			end
-		end
-	})
+	setmetatable(engine, meta)
 
 	return self
 end
