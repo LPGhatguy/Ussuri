d9909724b749d6cccdb7d4131e7d0978425bcc27
Lucien Greathouse <lpghatguy@lpghatguy.com>
2013-01-21 19:43:12 -0700

    Make the state machine work!

--- ussuri/misc/state_machine.lua
+++ ussuri/misc/state_machine.lua
@@ -7,11 +7,34 @@ local state = {}
 local lib
 
 state.event = {}
-state.states = {}
+state.state = ""
 state.handlers = {}
+
+setmetatable(state.event, {
+	__index = function(self, key)
+		local event = function(machine, ...)
+			local handlers = machine.handlers[machine.state]
+
+			if (handlers) then
+				local method = handlers[key]
+
+				if (method) then
+					method(machine, ...)
+				end
+			end
+		end
+
+		if (key) then
+			self[key] = event
+		end
+		return event
+	end
+})
 
 state.init = function(self, engine)
 	lib = engine.lib
+
+	lib.oop:objectify(self)
 
 	return self
 end
