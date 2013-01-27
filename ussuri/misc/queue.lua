755289d3b19d54ef6cad3c4e879ed8d21ec3c1b3
Lucien Greathouse <lpghatguy@lpghatguy.com>
2013-01-26 19:08:13 -0700

    Add queue (on top of state_machine)

---
+++ ussuri/misc/queue.lua
@@ -0,0 +1,73 @@
+local queue = {}
+local lib, table_pop
+
+queue.time = 0
+queue.elapsed = 0
+queue.acting = true
+queue.enabled = true
+queue.stack = {}
+
+queue.queue_update = function(self, event)
+	if (self.enabled) then
+		if (self.acting) then
+			self.elapsed = self.elapsed + event.delta
+
+			if (self.elapsed > self.time) then
+				self.elapsed = 0
+				self:queue_step()
+			end
+		elseif (#self.stack > 0) then
+			self:queue_step()
+		end
+	end
+end
+
+queue.queue_step = function(self)
+	local stack = self.stack
+
+	if (#stack > 0) then
+		local action = table_pop(self.stack)
+
+		local state = table_pop(action)
+		local time = tonumber(table_pop(action))
+		local method = table_pop(action)
+
+		acting = true
+		if (state) then
+			self:set_state(state)
+		end
+
+		if (time) then
+			self.time = time
+		end
+
+		if (method) then
+			method(unpack(action))
+		end
+	else
+		acting = false
+		self:set_state("")
+	end
+end
+
+queue.queue = function(self, ...)
+	for key, value in next, {...} do
+		table.insert(self.stack, value)
+	end
+end
+
+queue.post = {
+	update = queue.queue_update
+}
+
+queue.init = function(self, engine)
+	lib = engine.lib
+	table_pop = lib.utility.table_pop
+
+	lib.oop:objectify(self)
+	self:inherit(lib.misc.state_machine)
+
+	return self
+end
+
+return queue
