1e6435e4878756bece2945bb0535a50ca8aa79d3
Lucien Greathouse <lpghatguy@lpghatguy.com>
2013-01-27 17:43:37 -0700

    Make the fader less janky
    The demo's fader will be made a library component in engine version 1.3

--- demo/demo.lua
+++ demo/demo.lua
@@ -13,12 +13,12 @@ function love.load()
 	local title_graphic = love.graphics.newImage("demo/asset/image/title_graphic.png")
 
 	fader = lib.misc.queue:new()
-	fader.alpha = 255
+	fader.color = {0, 0, 0, 255}
 	fader.width = love.graphics.getWidth()
 	fader.height = love.graphics.getHeight()
 
 	fader.draw = function(self)
-		love.graphics.setColor(0, 0, 0, math.min(self.alpha, 255))
+		love.graphics.setColor(self.color)
 		love.graphics.rectangle("fill", 0, 0, self.width, self.height)
 	end
 
@@ -26,13 +26,19 @@ function love.load()
 		["in"] = {
 			draw = fader.draw,
 			update = function(self, event)
-				self.alpha = 270 - (270 * (self.elapsed / self.time))
+				self.color[4] = 255 - (255 * (self.elapsed / self.time))
+			end,
+			state_changing = function(self)
+				self.color[4] = 0
 			end
 		},
 		["out"] = {
 			draw = fader.draw,
 			update = function(self, event)
-				self.alpha = 270 * (self.elapsed / self.time)
+				self.color[4] = 255 * (self.elapsed / self.time)
+			end,
+			state_changing = function(self)
+				self.color[4] = 255
 			end
 		},
 		["wait"] = {
@@ -44,7 +50,7 @@ function love.load()
 	machine.handlers = {
 		["intro"] = {
 			state_changed = function(self)
-				fader:queue({"wait", 0.5}, {"in", 1}, {"wait", 2}, {"out", 1}, {"wait", 0.5, self.set_state, self, "title"})
+				fader:queue({"wait", 0.5}, {"in", 1}, {"wait", 2}, {"out", 0.5}, {"wait", 0.5, self.set_state, self, "title"})
 			end,
 			draw = function(self)
 				love.graphics.setColor(255, 255, 255)
@@ -53,7 +59,7 @@ function love.load()
 		},
 		["title"] = {
 			state_changed = function(self)
-				fader:queue({"in", 1})
+				fader:queue({"in", 0.5})
 			end,
 			draw = function(self)
 				love.graphics.setColor(255, 255, 255)
