eb6bfa669bb98bb1f3434a51e06db2fb10d4bede
Lucien Greathouse <lpghatguy@lpghatguy.com>
2013-03-02 21:42:10 -0700

    Remove the fader demo and assets

--- demo/demo.lua
+++ demo/demo.lua
@@ -8,19 +8,6 @@ local ussuri = require("ussuri")
 function love.load()
 	local lib = ussuri.lib
 
-	local drawer = {
-		event = {
-			draw = function(self)
-				love.graphics.rectangle("fill", 50, 50, 50, 50)
-			end,
-			joydown = function(self, event)
-				print("Player", event.joystick, "pressed", event.button)
-			end,
-		}
-	}
-
-	ussuri:event_hook(nil, drawer)
-
 	ussuri:event_hook(nil, lib.debug.header)
 	ussuri:event_hook(nil, lib.debug.monitor)
 	ussuri:event_hook(nil, lib.debug.console)
