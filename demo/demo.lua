5b1c8cb1bb4a4e6d39d2d286c1a3a5be612ae10f
Lucien Greathouse <lpghatguy@lpghatguy.com>
2013-03-05 22:59:49 -0700

    Add a frame stepper (this needs a better name)

--- demo/demo.lua
+++ demo/demo.lua
@@ -8,16 +8,6 @@ local ussuri = require("ussuri")
 function love.load()
 	local lib = ussuri.lib
 
-	local container = lib.ui.frame:new()
-	container.x = 50
-	container.y = 50
-	container.width = 200
-	container.height = 200
-	lib.debug.console.environment.container = container
-
-	local textbox = lib.ui.textbox_multiline:new()
-	container:register("textbox", textbox)
-
 	ussuri:event_hook(nil, lib.debug.header)
 	ussuri:event_hook(nil, lib.debug.monitor)
 	ussuri:event_hook(nil, lib.debug.console)
