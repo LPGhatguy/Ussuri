25f0ce1c26401f32e8bd8aa726fb3b01878e9960
Lucien Greathouse <lpghatguy@lpghatguy.com>
2013-03-05 22:59:07 -0700

    Add generic event triggering

--- ussuri/utility/container.lua
+++ ussuri/utility/container.lua
@@ -21,7 +21,26 @@ container = {
 		self.children[key] = nil
 	end,
 
-	
+	trigger_child_event = function(self, child, event_name, event_pass)
+		if (child[event_name]) then
+			child[event_name](child, event_pass)
+		elseif (child.trigger_event) then
+			child:trigger_event(event_name, event_pass)
+		end
+	end,
+
+	trigger_event = function(self, event_name, event_pass)
+		local stack = event_pass.stack
+		stack[#stack + 1] = self
+		event_pass.up = self
+
+		for key, child in next, self.children do
+			self:trigger_child_event(child, event_name, event_pass)
+		end
+
+		stack[#stack] = nil
+		event_pass.up = stack[#stack]
+	end,
 
 	init = function(self, engine)
 		lib = engine.lib
