c908aa4eb51c912738c197f7360c1b463a048c9a
Lucien Greathouse <lpghatguy@lpghatguy.com>
2013-05-05 14:54:55 -0600

    Up version to 1.2.1.DEV
    This is the end of the LUDUM-DEV merges (the last three commits) which
    helped expose a few key issues in some of the engine's core.

--- ussuri/config.lua
+++ ussuri/config.lua
@@ -1,5 +1,5 @@
 return {
-	version = {1, 2, 0, "DEV"},
+	version = {1, 2, 1, "DEV"},
 
 	lib_core = {":core.utility", ":core.oop", ":core.logging", ":core.lib_manage"},
 	lib_folders = {":event", ":misc", ":input", ":debug"}
