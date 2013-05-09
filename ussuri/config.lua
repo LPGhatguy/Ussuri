f176b176af7563d3bff86d4f8636be464541b6c4
Lucien Greathouse <lpghatguy@lpghatguy.com>
2013-05-08 22:19:42 -0600

    Update core load order and version

--- ussuri/config.lua
+++ ussuri/config.lua
@@ -1,6 +1,6 @@
 return {
-	version = {1, 2, 1, "DEV"},
+	version = {1, 2, 2, "CORE-DEV"},
 
 	lib_core = {":core.utility", ":core.oop", ":core.logging", ":core.lib_manage"},
-	lib_folders = {":event", ":misc", ":input", ":debug"}
+	lib_folders = {":event", ":misc", ":input", ":debug", ":ui"}
 }
