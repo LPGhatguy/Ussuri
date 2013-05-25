3b3a1b99e7ecb1f8ca20637640d75a85ae7ed960
Lucien Greathouse <lpghatguy@lpghatguy.com>
2013-05-24 22:59:56 -0600

    Change version string

--- ussuri/config.lua
+++ ussuri/config.lua
@@ -1,5 +1,5 @@
 return {
-	version = {1, 2, 2, "CORE-DEV"},
+	version = {1, 2, 2, "STABLE"},
 
 	lib_core = {":core.utility", ":core.oop", ":core.logging", ":core.lib_manage"},
 	lib_folders = {":event", ":misc", ":input", ":debug", ":ui"}
