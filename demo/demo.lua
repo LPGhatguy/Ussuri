228ae8d84dd2f5bd9749a532e8ec941c520f5ab7
Lucien Greathouse <lpghatguy@lpghatguy.com>
2013-05-24 22:45:39 -0600

    Update the demo to use the new event container

--- demo/demo.lua
+++ demo/demo.lua
@@ -8,15 +8,10 @@ local ussuri = require("ussuri")
 function love.load()
 	local lib = ussuri.lib
 
-	local container = lib.event.container:new()
+	local container = lib.ui.ui_container:new()
 
 	local ui_item = lib.ui.base:new()
-	ui_item.draw = function(self)
-		love.graphics.rectangle("fill", 50, 50, 50, 50)
-	end
 
-	container:event_create("draw")
-	container.auto_hook.draw = true
 	container:add(ui_item)
 
 	ussuri.event:event_hook_object("draw", container)
