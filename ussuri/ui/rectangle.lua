7a3e26c561dd1d2aa0e70758f02768b07b9d8731
Lucien Greathouse <me@lpghatguy.com>
2013-05-26 00:40:51 -0600

    I present to you... the rectangle class!

--- ussuri/ui/rectangle.lua
+++ ussuri/ui/rectangle.lua
@@ -7,8 +7,26 @@ local lib
 local rectangle
 
 rectangle = {
+	background_color = {50, 50, 50, 255},
+	border_color = {120, 120, 120, 255},
+
+	border_width = 2,
+
 	draw = function(self)
+		love.graphics.setColor(self.background_color)
 		love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+
+		local border = self.border_width
+		local half_border = border / 2
+
+		love.graphics.setColor(self.border_color)
+		love.graphics.setLineWidth(border)
+
+		if (border % 2 == 0) then
+			love.graphics.rectangle("line", self.x - half_border, self.y - half_border, self.width + border, self.height + border)
+		else
+			love.graphics.rectangle("line", self.x - half_border, self.y - half_border, self.width + border, self.height + border)
+		end
 	end,
 
 	init = function(self, engine)
