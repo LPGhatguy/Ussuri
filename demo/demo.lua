4c130c1f09e6945e960de329b448a6e0510df2a8
Lucien Greathouse <me@lpghatguy.com>
2013-06-09 21:10:20 -0600

    Update demo

--- demo/demo.lua
+++ demo/demo.lua
@@ -1,6 +1,5 @@
 --[[
-Ussuri 1.2.2 Demo!
-Featuring UI elements!
+Ussuri 1.3.0 Demo
 ]]
 
 local ussuri = require("ussuri")
@@ -8,13 +7,17 @@ local ussuri = require("ussuri")
 function love.load()
 	local lib = ussuri.lib
 
-	local container = lib.ui.ui_container:new(50, 50, 1024, 768)
+	local container = lib.ui.ui_container:new(0, 0, 1024, 768)
+	container.clip_children = false
+
+	local subcont = lib.ui.frame:new(50, 0, 180, 200)
 
 	local ui_item = lib.ui.button:new(50, 50, 50, 50)
 
 	local image = lib.ui.image:new(love.graphics.newImage("demo/asset/fun_block.png"), 100, 50, 50, 50)
 
-	container:adds({ui_item, image})
+	container:add(subcont)
+	subcont:adds({ui_item, image})
 
 	ussuri.event:event_hook_object(nil, container)
 
