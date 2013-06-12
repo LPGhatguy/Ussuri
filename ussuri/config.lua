19cbb8dd043929799846abaa29c2dbd1ad499b71
Lucien Greathouse <me@lpghatguy.com>
2013-06-12 06:44:23 -0600

    Increment version!

--- ussuri/config.lua
+++ ussuri/config.lua
@@ -1,5 +1,5 @@
 return {
-	version = {1, 3, 0, "CORE-DEV"},
+	version = {1, 3, 1, "CORE-DEV"},
 
 	lib_core = {":core.utility", ":core.oop", ":core.logging", ":core.lib_manage"},
 	lib_folders = {":event", ":misc", ":input", ":debug", ":ui"}
