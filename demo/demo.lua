46f96f767f3d57582807242dcf7b51b1a1fd571f
Lucien Greathouse <me@lpghatguy.com>
2013-06-12 06:44:50 -0600

    Bare minimum demo

--- demo/demo.lua
+++ demo/demo.lua
@@ -7,22 +7,7 @@ local ussuri = require("ussuri")
 function love.load()
 	local lib = ussuri.lib
 
-	local container = lib.ui.ui_container:new(0, 0, 1024, 768)
-	container.clip_children = false
-
-	local subcont = lib.ui.frame:new(50, 0, 180, 200)
-
-	local ui_item = lib.ui.button:new(50, 50, 50, 50)
-
-	local image = lib.ui.image:new(love.graphics.newImage("demo/asset/fun_block.png"), 100, 50, 50, 50)
-
-	container:add(subcont)
-	subcont:adds({ui_item, image})
-
-	ussuri.event:event_hook_object(nil, container)
-
-	ussuri.event:event_hook_object(nil, lib.debug.header)
-	ussuri.event:event_hook_object(nil, lib.debug.monitor)
-
+	local ui_root = lib.ui.ui_container:new()
+	ussuri.event:event_hook_object(nil, ui_root)
 	ussuri.event:event_sort()
 end
