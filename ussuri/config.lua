c428863fa08f341f65cc4d4ca1f3e62bd3ee36fc
Lucien Greathouse <me@lpghatguy.com>
2013-06-15 02:18:56 -0600

    lib_folders renamed to lib_aux

--- ussuri/config.lua
+++ ussuri/config.lua
@@ -2,5 +2,5 @@ return {
 	version = {1, 3, 1, "CORE-DEV"},
 
 	lib_core = {":core.utility", ":core.oop", ":core.logging", ":core.lib_manage"},
-	lib_folders = {":event", ":misc", ":input", ":debug", ":ui"}
+	lib_aux = {":event", ":misc", ":input", ":debug", ":ui"}
 }
