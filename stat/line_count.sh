1b987a62497b63a977955e7ce2a7a004520d1ff1
Lucien Greathouse <me@lpghatguy.com>
2013-06-17 01:10:09 -0600

    Line count is in Lua as debug.meta now

--- stat/line_count.sh
+++
@@ -1,3 +0,0 @@
-cd ..
-git ls-files | xargs cat | wc -l
-read -p "End."
--- ussuri/debug/meta.lua
+++ ussuri/debug/meta.lua
@@ -4,12 +4,35 @@ Gives information about the engine and its workings
 ]]
 
 local lib
-local stat
+local meta
 
-stat = {
+meta = {
+	code = function(self, engine, dir)
+		local dir = dir or engine.config.path
+		local file_count = 0
+		local line_count = 0
+
+		for key, filename in pairs(love.filesystem.enumerate(dir)) do
+			local path = dir .. filename
+
+			if (love.filesystem.isFile(path)) then
+				file_count = file_count + 1
+				for line in love.filesystem.lines(path) do
+					line_count = line_count + 1
+				end
+			else
+				local files, lines = self:code(engine, path .. "/")
+				file_count = file_count + files
+				line_count = line_count + lines
+			end
+		end
+
+		return file_count, line_count
+	end,
+
 	init = function(self, engine)
 		lib = engine.lib
 	end
 }
 
-return stat
+return meta
