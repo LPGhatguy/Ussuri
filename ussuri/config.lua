7b0741d3ad912031ff2e9966087caf23aba91a0b
Lucien Greathouse <me@lpghatguy.com>
2013-06-17 05:11:47 -0600

    Update configuration
    -Increment version to 1.3.3 DEV
    -Add data and content modules
    -Add author_language field, default to "en"

--- ussuri/config.lua
+++ ussuri/config.lua
@@ -1,6 +1,8 @@
 return {
-	version = {1, 3, 2, "DEV"},
+	version = {1, 3, 3, "DEV"},
 
 	lib_core = {":core.utility", ":core.oop", ":core.logging", ":core.lib_manage"},
-	lib_aux = {":event", ":misc", ":input", ":debug", ":ui"}
+	lib_aux = {":event", ":misc", ":input", ":debug", ":ui", ":data", ":content"},
+
+	author_language = "en"
 }
